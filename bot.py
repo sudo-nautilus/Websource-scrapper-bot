75b355ee530088017a28dd9be2be6dd20d78c819
Maday <76758776+heartlog@users.noreply.github.com>
2021-10-11 04:19:06 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,11 @@
+
+import os
+import aiohttp
+from configs import Config
+from bs4 import BeautifulSoup
+from pyrogram import Client
+from pyrogram.types import *
+
+Bot = Client("Bakascrapper", bot_token=Config.BOT_TOKEN, api_id=Config.API_ID, api_hash=Config.API_HASH, plugins={"root": "plugins"})
+
+Bot.run()
