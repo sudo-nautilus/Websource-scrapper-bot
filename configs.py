88978d046e45c00e30ee1c2764008c01c0ffc89f
Maday <76758776+heartlog@users.noreply.github.com>
2021-10-11 04:20:23 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,6 +3,6 @@
 import os
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID", "2176353"))
-	API_HASH = os.environ.get("API_HASH", "9265189e6b3c63d54bf637256fe78523")
+	API_ID = int(os.environ.get("API_ID", "1234"))
+	API_HASH = os.environ.get("API_HASH", "1234")
 	BOT_TOKEN = os.environ.get("BOT_TOKEN")
