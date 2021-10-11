d85f04df0d5d5f27dd491bdfc3f9b8f315557aea
Maday <76758776+heartlog@users.noreply.github.com>
2021-10-11 19:50:46 +0530

    Update source.py

--- plugins/source.py
+++ plugins/source.py
@@ -1,3 +1,4 @@
+# Basic bs4 and requests extraction...
 import os
 import asyncio
 import requests
@@ -8,17 +9,25 @@ from pyrogram.types import *
 @Client.on_message(filters.command('source') & filters.private)
 async def start(bot, cmd):
     txt = cmd.text
+    me = await bot.get_me()
     if len(txt) > 8:
         RURL = txt.split(" ")[1]
-        page = = requests.get(RURL)
-        soup = BeautifulSoup(page.content, "html.parser")
-        
-        with open(f"web-baka-scrapper-{cmd.from_user.id}.txt", "w+") as file:
-            file.write(soup)
+        a = await cmd.reply_text(f"Wait **Baka** I am scrapping the webpage `{RURL}` ...")
         try:
-        await cmd.reply_document("web-baka-scrapper-{cmd.from_user.id}.txt")
-
-        remove(f"web-baka-scrapper-{cmd.from_user.id}.txt")
+            page = requests.get(RURL)
+            soup = BeautifulSoup(page.content, "html.parser")
+            pre = soup.prettify()
+            strsoup = str(pre)
+            name = f"Web-baka-scrapper-{cmd.from_user.id}.txt"
+            with open(name, 'w') as f:
+                f.write(strsoup)
+            cap = f"Source: [->]({RURL})\nFrom {me.mention} baka !"
+            await cmd.reply_document(document=name, caption=cap, quote=True)
+            await a.edit("Completed !!")
+            await a.delete()
+        except:
+            await a.edit("Something is wrong with your link 😑\nMake sure that's a webpage link...")
+        remove(name)
     else:
-        await cmd.reply_sticker("CAACAgIAAxkBAAJtdWEJ8kFVLOTcJZkoO4AgLEQiqhjVAAIpKwAC4KOCBxOA6A0D4-_jIAQ", quote=True)
-        await cmd.reply_text("No query please give streamtape vaild link")
+        await cmd.reply_sticker("CAACAgUAAxkBAAEMHjphY-XBDkah-iAgca0nflKX3arZqAACIgQAAthVoVXewVW3WmMkcSEE", quote=True)
+        await cmd.reply_text("Just paste your link in /source `[webpage link]`")
