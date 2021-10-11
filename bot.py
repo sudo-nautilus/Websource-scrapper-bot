8ca046145e1eb6fada6e2c394dda43c3f6ebdfcf
Maday <76758776+heartlog@users.noreply.github.com>
2021-10-11 19:46:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,10 @@
-
+# copyright @heartlog
 import os
 import aiohttp
 from configs import Config
 from bs4 import BeautifulSoup
 from pyrogram import Client
 from pyrogram.types import *
-cloudscraper
 
 Bot = Client("Bakascrapper", bot_token=Config.BOT_TOKEN, api_id=Config.API_ID, api_hash=Config.API_HASH, plugins={"root": "plugins"})
 
